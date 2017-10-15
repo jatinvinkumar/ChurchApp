445bc9eafd5534993ca18f380c8139161cce04ea
jatinvinkumar <jatinvinkumar@gmail.com>
2017-10-15 12:48:01 -0500

    Initial Commit

---
+++ Pray/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pray
+//
+//  Created by Jatin K on 10/15/17.
+//  Copyright © 2017 Jatin K. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
